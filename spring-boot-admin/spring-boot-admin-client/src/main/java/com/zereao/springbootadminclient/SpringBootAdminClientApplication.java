7620d0499a93274ed4fbba498a8278cb43bec2a9
heyulun <heyulun@sunlands.com>
2019-01-10 19:52:26 +0800

    HelloWorld

---
+++ spring-boot-admin/spring-boot-admin-client/src/main/java/com/zereao/springbootadminclient/SpringBootAdminClientApplication.java
@@ -0,0 +1,14 @@
+package com.zereao.springbootadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientApplication.class, args);
+    }
+
+}
+
