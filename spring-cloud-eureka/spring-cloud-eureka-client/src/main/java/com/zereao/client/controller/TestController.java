5cb2277c1bfdc643833f1a254522e4d2036f6c63
Zereao <1050214520@qq.com>
2019-02-19 14:43:37 +0800

    H

---
+++ spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/zereao/client/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.zereao.client.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Darion Mograine H
+ * @version 2019/02/19  11:33
+ */
+@RestController
+public class TestController {
+    @GetMapping("hi")
+    public String test(@RequestParam("name") String param) {
+        System.out.println("============== param = " + param + " ==============");
+        return "SUCCESS";
+    }
+}
