a925169d291de01f4c34429527f8e0297128d226
“Zereao” <1050214520@qq.com>
2019-05-27 20:43:05 +0800

    引入 SpringSecurity Oauth2 module，调整 pom 依赖

---
+++ spring-security/spring-security-oauth2/src/main/java/com/zereao/oauth2/SecurityOauth2App.java
@@ -0,0 +1,13 @@
+package com.zereao.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauth2App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOauth2App.class, args);
+    }
+
+}
---
+++ spring-security/spring-security-oauth2/src/test/java/com/zereao/springsecurityoauth2/SecurityOauth2AppTests.java
@@ -0,0 +1,16 @@
+package com.zereao.springsecurityoauth2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityOauth2AppTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
